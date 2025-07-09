538fdf7c30eb62a1c8ceeb63cbbcdb6495ea346c
javilujann <jp.lujanromero@um.es>
2025-07-09 10:26:55 +0200

    Modularizar la función main para soportar el modo de extracción de comentarios

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,25 @@
 import 'dotenv/config'; // Cargar variables de entorno
-import { extractCommentsPuppeteer } from './extractors';
+import {
+  extractCommentsPuppeteer,
+  extractCommentsBridgeMode,
+} from './extractors';
 import { generateStats } from './analyzers';
 import { filterComments } from './utils';
 
-async function main(newsUrl: string) {
+async function main(newsUrl: string, useBridgeMode: boolean = false) {
   console.log('🎯 News Analyzer - PoC');
   console.log('========================\n');
 
-  // Validar URL
   try {
-    new URL(newsUrl);
-  } catch {
-    console.error('❌ URL inválida:', newsUrl);
-    return;
-  }
-
-  try {
-    // Extraer comentarios
-    let comments = await extractCommentsPuppeteer(newsUrl);
+    // Extraer comentarios usando el modo seleccionado
+    let comments;
+    if (useBridgeMode) {
+      console.log('🌉 Usando Bridge Mode...');
+      comments = await extractCommentsBridgeMode(newsUrl);
+    } else {
+      console.log('🐶 Usando Puppeteer...');
+      comments = await extractCommentsPuppeteer(newsUrl);
+    }
 
     // Validar y limpiar datos
     comments = filterComments(comments);
@@ -32,11 +34,28 @@ async function main(newsUrl: string) {
 }
 
 // Ejecutar el script con la URL de noticias
-const newsUrl = process.argv[2];
+const args = process.argv.slice(2);
+
+const newsUrl = args[0];
+const useBridgeMode = args[1] === 'bridge' ? true : false;
+
+
+// Validar Argumentos
 if (!newsUrl) {
   console.error(
     '❌ Por favor, proporciona una URL de noticias como argumento.'
   );
+  console.log('Uso: npm start <url> [bridge]');
+  console.log('Ejemplo: npm start "https://noticia.com" bridge');
   process.exit(1);
 }
-main(newsUrl);
+
+// Validar URL
+try {
+  new URL(newsUrl);
+} catch {
+  console.error('❌ URL inválida:', newsUrl);
+  process.exit(1);
+}
+
+main(newsUrl, useBridgeMode);
